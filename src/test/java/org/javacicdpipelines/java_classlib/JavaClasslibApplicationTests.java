0f96d4751bc91e833fc179c9175791c53d315064
Andressa Fontes <andressafc477@gmail.com>
2025-06-06 12:04:30 -0300

    Initialize Java class library project

---
+++ src/test/java/org/javacicdpipelines/java_classlib/JavaClasslibApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javacicdpipelines.java_classlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaClasslibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
