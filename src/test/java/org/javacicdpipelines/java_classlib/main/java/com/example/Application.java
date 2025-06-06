56bfec3f50758f0bfd3fe3f20933567a0b2d9aeb
Andressa Fontes <andressafc477@gmail.com>
2025-06-06 12:13:34 -0300

    Initialize Application class and application properties

---
+++ src/test/java/org/javacicdpipelines/java_classlib/main/java/com/example/Application.java
@@ -0,0 +1,13 @@
+package org.javacicdpipelines.java_classlib.main.java.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
